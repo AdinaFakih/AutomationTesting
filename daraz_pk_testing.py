c87d35bf041b35b0afbc039ddb2b62280019045e
AdinaFakih <addy.fakih@gmail.com>
2021-06-09 13:22:04 +0500

    Day 8 progress

--- daraz_pk_testing.py
+++ daraz_pk_testing.py
@@ -5,28 +5,42 @@ from selenium.webdriver import ActionChains
 import time
 driver=webdriver.Chrome(executable_path="C:\webdriver\chromedriver.exe")
 driver.maximize_window()
-driver.get("https://www.daraz.pk/") #front page
+#driver.get("https://www.daraz.pk/") #front page
 #driver.get("https://member.daraz.pk/user/register?spm=a2a0e.login_signup.header.d6.46cc7d68SvNZdo") # Undo this line, this is direct signup page
 
-time.sleep(2)
-print(driver.title) #get title
-driver.find_element_by_xpath("//*[@id='anonSignup']/a").click() #click sign up page
-driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[1]/div[1]/input").send_keys("12345678910")
-driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[2]/div[1]/input").send_keys("Adi")
-silder = driver.find_element_by_id("nc_2_n1z")
+time.sleep(5)
+driver.get("https://www.daraz.pk/smartphones/apple/?page=1&sort=pricedesc&spm=a2a0e.home.cate_1_1.2.7f814937DINRnb&style=list")
+time.sleep(5)
 
+#scrolling to the Warranty Type section, using pixels
+driver.execute_script("window.scrollBy(0, 1600)","")
+time.sleep(20)
+
+'''
+print(driver.title) #get title
+time.sleep(3)
+driver.find_element_by_xpath("//*[@id='anonSignup']/a").click() #click sign up page
+time.sleep(3)
+driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[1]/div[1]/input").send_keys("12345678910")
+time.sleep(3)
+driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[2]/div[1]/input").send_keys("Adi")
+time.sleep(3)
+silder = driver.find_element_by_id("nc_2_n1z")
+time.sleep(3)
 act = ActionChains(driver)
 act.drag_and_drop_by_offset(silder, 342,0)
 act.perform()
 time.sleep(3)
 driver.find_element_by_xpath("/html/body/div[8]/div/div/div[3]/button").click() #click okay
-
+time.sleep(3)
 #checking password is avalible
 pwds_element = driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[1]/div[3]/input")
 print(pwds_element.is_displayed())
 print(pwds_element.is_enabled())
-
-driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[2]/div[3]/button").click() #signup button
-
 time.sleep(5)
+driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[2]/div[3]/button").click() #signup button
+'''
+
+
+time.sleep(9)
 driver.quit()
--- testing_daraz.py
+++ testing_daraz.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+
 import time
 import logging
 
 #path of Chrome wweb driver
 driver = webdriver.Chrome(executable_path="C:\webdriver\chromedriver.exe")
+#delete this line later
+driver.maximize_window()
+
 
 #logs are saved to file for easy read
 logging.basicConfig(filename="C://Users//addyf//Desktop//Certificates//test.log",
@@ -19,48 +22,57 @@ logger.setLevel(logging.INFO)
 
 #miximize the window
 driver.maximize_window()
-time.sleep(2)
+time.sleep(5)
 logger.info("Browser Window is maximized")
 
 #Opening daraz.pk website
 driver.get("https://www.daraz.pk/")
-time.sleep(2)
+#time.sleep(6)
 logger.info("Website is opened")
 
 #Verifying the title name
 assert "Online Shopping in Pakistan: Fashion, Electronics & Books - Daraz.pk" in driver.title  #conform
 
-
 cookies = driver.get_cookies() #Capture all the cookies created by browser
 print(len(cookies))  #print number of cookies have been created
 print(cookies) #print all the cookies pairs
 
+#uncomment
+
 #mouse hover actions to navigate
 devices = driver.find_element_by_xpath('//*[@id="Level_1_Category_No1"]/a/span')
-time.sleep(2)
+time.sleep(5)
 mobile = driver.find_element_by_xpath('//*[@id="J_5022174600"]/div/ul/ul[1]/li[1]/a/span')
-time.sleep(2)
+time.sleep(5)
 iphone = driver.find_element_by_xpath('//*[@id="J_5022174600"]/div/ul/ul[1]/li[1]/ul/li[2]/a/span')
-time.sleep(2)
+time.sleep(5)
 actions = ActionChains(driver)
 actions.move_to_element(devices).move_to_element(mobile).move_to_element(iphone).click().perform()
 time.sleep(4)
 
 #undo this line, its is a direct link to iphone page
 #driver.get("https://www.daraz.pk/smartphones/apple/?spm=a2a0e.home.cate_1_1.2.66294937SnuF7j&style=list")
-time.sleep(2)
+time.sleep(4)
 #another way of view all items, individual items
 driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[1]/div/div[1]/div[1]/div/div[2]/div/div[4]/span[2]/i").click()
+time.sleep(8)
+
+#Sorting, Highest to lowest price
+driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[1]/div/div[1]/div[1]/div/div[2]/div/div[2]/div").click()
 time.sleep(5)
-#scrolling to a specific item
-flag = driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[1]/div/div[1]/div[3]/div[7]/div/div[2]/div[2]/a")
+driver.find_element_by_xpath("/html/body/div[5]/div/div/div/ul/li[3]/div").click()  #/html/body/div[6]/div/div/div/ul/li[3]
+time.sleep(8)
+
+#scrolling to a specific item that you want
+flag = driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[1]/div/div[1]/div[3]/div[3]/div/div[1]/div/div[1]/a")
 driver.execute_script("arguments[0].scrollIntoView();",flag)
 time.sleep(9)
-#selecting an item
-driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[1]/div/div[1]/div[3]/div[8]/div/div[1]/div/div[1]/a/img").click()
-time.sleep(2)
 
-driver.save_screenshot("C:/Users/addyf/Desktop/Certificates/selected_item_img.png")
+#selecting an item that you want
+driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[1]/div/div[1]/div[3]/div[3]/div/div[1]/div/div[1]/a/img").click()
+time.sleep(9)
+
+#driver.save_screenshot("C:/Users/addyf/Desktop/Certificates/selected_item_img.png")
 
 
 #undo this line, its is a direct link to the selected item
@@ -70,27 +82,33 @@ time.sleep(2)
 #mouse actions to CHANGE COLOURS of selected item
 for i in range(1,5):
     color = driver.find_element_by_xpath(f'/html/body/div[4]/div/div[2]/div[2]/div/div[1]/div[12]/div/div[1]/div/div/div[2]/span[{i}]')
-    time.sleep(2)
+    time.sleep(3)
 
     actions = ActionChains(driver)
     actions.move_to_element(color).perform()
     time.sleep(2)
 
+time.sleep(3)
 #click on ADD TO CART
 driver.find_element_by_xpath("/html/body/div[4]/div/div[2]/div[2]/div/div[1]/div[15]/div/button[2]/span/span").click()
-time.sleep(2)
+time.sleep(9)
 #back to items page because login frames are not there
 driver.back()
+time.sleep(9)
+
 #forward to selected item
 driver.forward()
+time.sleep(9)
 
 #scrolling to the comments section, using pixels
 driver.execute_script("window.scrollBy(0,2700)","")
-for i in range(1,6):
+time.sleep(5)
+for i in range(1,4):  #6
     driver.find_element_by_xpath(f"/html/body/div[4]/div/div[8]/div[1]/div[3]/div/div[2]/div[2]/div[2]/div/div/button[{i}]").click()
     time.sleep(4)
 
 driver.back()
+time.sleep(9)
 
 '''#scrolling to the comments section, using xpath
 flag = driver.find_element_by_xpath("/html/body/div[4]/div/div[8]/div[1]/div[3]/div/div[2]/div[2]/div[2]/div/button[1]/i")
@@ -98,47 +116,56 @@ driver.execute_script("arguments[0].scrollIntoView();",flag)
 time.sleep(5)
 '''
 
-#scrolling to the color family section, using pixels
-driver.execute_script("window.scrollBy(0,-1000)","")
 
-#driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[1]/div/div[2]/div/div[11]/div[2]/div/div/label[1]/span[2]").click()
+time.sleep(12)
+driver.save_screenshot("C:/Users/addyf/Desktop/Certificates/before_scroll_img.png")
+time.sleep(12)
+#scrolling to the Warranty Type section, using pixels
+driver.execute_script("window.scrollBy(0,500)","")
+time.sleep(12)
+driver.save_screenshot("C:/Users/addyf/Desktop/Certificates/after_scroll_img.png")
+time.sleep(12)
+#driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[1]/div/div[2]/div/div[11]/div[2]/div/div/label[1]/span[2]").click() #color family
 
-#first check if radio box is selected brand
+#first check if radio box is selected or not for Brand feature
 result = driver.find_element_by_xpath('/html/body/div[3]/div/div[2]/div[1]/div/div[2]/div/div[11]/div[2]/div/div/label[1]/span[2]').is_selected()
 if result:
     print('Checkbox already selected')
 else:
     driver.find_element_by_xpath('/html/body/div[3]/div/div[2]/div[1]/div/div[2]/div/div[11]/div[2]/div/div/label[1]/span[2]').click()
     print("Checkbox selected")
-time.sleep(5)
+time.sleep(9)
 
 #Search for bags
 driver.find_element_by_id("q").send_keys("bags")
-time.sleep(1)
+time.sleep(9)
 driver.find_element_by_id("q").send_keys(Keys.ENTER)
-time.sleep(3)
+time.sleep(9)
 
-driver.find_element_by_xpath("//*[@id='anonSignup']/a").click() #click sign up page
+#undo this line later
+#driver.get("https://www.daraz.pk/catalog/?q=bags&_keyori=ss&from=input&spm=a2a0e.searchlistcategory.search.go.6660286cpdVtXK")
+
+driver.find_element_by_id("anonSignup").click() #click sign up page           //*[@id='anonSignup']/a
+time.sleep(9)
 driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[1]/div[1]/input").send_keys("12345678910")
-time.sleep(1)
+time.sleep(4)
 driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[2]/div[1]/input").send_keys("Adi")
-time.sleep(1)
+time.sleep(4)
 silder = driver.find_element_by_id("nc_2_n1z")
-
+time.sleep(4)
 act = ActionChains(driver)
 act.drag_and_drop_by_offset(silder, 342,0)
 act.perform()
 time.sleep(3)
 driver.find_element_by_xpath("/html/body/div[8]/div/div/div[3]/button").click() #click okay
-
+time.sleep(5)
 #checking password is avalible
 pwds_element = driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[1]/div[3]/input")
 print(pwds_element.is_displayed())
+time.sleep(2)
 print(pwds_element.is_enabled())
-
+time.sleep(9)
 driver.find_element_by_xpath("/html/body/div[2]/div/div[2]/div/div[2]/form/div/div[2]/div[3]/button").click() #signup button
 
-
-
-time.sleep(9)
-#driver.quit()
+time.sleep(20)
+driver.quit()
